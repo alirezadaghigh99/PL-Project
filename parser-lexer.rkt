fe697d8dfdad9f93985bca5f21d1517815eeb4a8
nima <nimxj4141@gmail.com>
2020-12-31 22:02:17 +0330

    parser-lexer.rkt updated

--- parser-lexer.rkt
+++ parser-lexer.rkt
@@ -10,34 +10,29 @@
   (lexer
    ["+" (token-plus)]
    ["-" (token-minus)]
-   ["*" (token-mul)]
-   ["/" (token-div)]
-   [">" (token-gt)]
-   ["<" (token-lt)]
-   ["<=" (token-lte)]
-   [">=" (token-gte)]
-   ["==" (token-eq)]
-   ["=" (token-seteq)]
-   
-   ["!=" (token-noteq)]
+   ["*" (token-multiply)]
+   ["/" (token-division)]
+   ["=" (token-assign)]
+   ["==" (token-equal)]
+   [">" (token-greater)]
+   ["<" (token-less)]
+   ["!=" (token-not-eq)]
    [";" (token-semicolon)]
    ["," (token-comma)]
-   ["[" (token-lbrack)]
-   ["]" (token-rbrack)]
+   ["[" (token-lbracket)]
+   ["]" (token-rbracket)]
    ["(" (token-lpar)]
    [")" (token-rpar)]
    ["{" (token-lcurly)]
    ["}" (token-rcurly)]
-   ["return" (token-ret)]
+   ["return" (token-return)]
    ["while" (token-while)]
-   ["do" (token-dot)]
-   ["end" (token-endwhile)]
+   ["do" (token-dowhile)]
+   ["end" (token-end)]
    ["if" (token-if)]
    ["then" (token-then)]
    ["else" (token-else)]
    ["null" (token-null null)]
-   ["endif" (token-eif)]
-   ["func" (token-func)]
    [(:or "true" "false") (token-bool (if (equal? lexeme "true") #t #f))]
    [(:+ (:or (char-range #\a #\z) (char-range #\A #\Z))) (token-var lexeme)]
    [(:or (:+ (char-range #\0 #\9)) (:: (:+ (char-range #\0 #\9)) #\. (:+ (char-range #\0 #\9)))) (token-num (string->number lexeme))]
@@ -48,49 +43,41 @@
   
    ))
 
+(define-tokens a (num string bool var null))
+(define-empty-tokens b (eof plus minus multiply division equal assign not-eq greater less semicolon comma lbracket rbracket lpar rpar lcurly rcurly return while dowhile end if then else))
+
 (define parser_compiler
   (parser
    (start command)
             (end eof)
-            (error (lambda (tok-ok? tok-name tok-value)
-                    (displayln tok-ok?)
-                     (displayln tok-name)
-                     (displayln tok-value)
-                     ;(displayln (error-thrower tok-name tok-value))
-
-                    ))
             (tokens a b)
+            (error void)
             (grammar
 
-             [command ((rcommand) (list 'rcommand $1)) ((rcommand semicolon command) (list 'lcommand $1 $3))]
+             [command ((keyword) (list 'keyword $1)) ((command semicolon keyword) (list 'command $1 $3))]
              
-             [whilecommand ((while exp dot command endwhile) (list 'while $2 $4))]
-             
-             [rcommand
-            ((whilecommand) (list 'whilecommand $1))
-            ((ifcommand) (list 'ifcommand $1))
-            ((assign) (list 'assigncommand $1))
-            ((return) (list 'returncommand $1))
+             [keyword
+            ((if-statement) (list 'if_statement $1))
+            ((while-statement) (list 'while_statement $1))
+            ((assignment-statement) (list 'assignment_statement $1))
+            ((return-statement) (list 'return $1))
             ]
              
-             [ifcommand ((if exp then command else command eif) (list 'if $2 $4 $6))]
+             [if-statement ((if exp then command else command end) (list 'if $2 $4 $6))]
 
-             [FUNCTION ((func lpar variables rpar lcurly command rcurly) (list 'function $3 $6))]
+             [while-statement ((while exp dowhile command end) (list 'while $2 $4))]
+          
            
-           [CALL ((var lpar args rpar) (list 'call-function $1 $3))]
+             [assignment-statement ((var assign exp) (list 'assign $1 $3))]
            
-           [assign
-            ((var seteq exp) (list 'seteq $1 $3))
-            ]
-           
-           [return ((ret exp) (list 'return $2))]
+             [return-statement ((return exp) (list 'return $2))]
            
            [exp
             ((aexp) (list 'aexp $1))
-            ((aexp gt aexp) (list 'greater? $1 $3))
-            ((aexp lt aexp) (list 'less? $1 $3))
-            ((aexp eq aexp) (list 'eqs? $1 $3))
-            ((aexp noteq aexp) (list 'noteqs? $1 $3))
+            ((aexp greater aexp) (list 'greater? $1 $3))
+            ((aexp less aexp) (list 'less? $1 $3))
+            ((aexp equal aexp) (list 'equals? $1 $3))
+            ((aexp not-eq aexp) (list 'not-equals? $1 $3))
             ]
            
            [aexp
@@ -101,56 +88,42 @@
            
            [bexp
             ((cexp) (list 'cexp $1))
-            ((cexp mul bexp) (list 'mult $1 $3))
-            ((cexp div bexp) (list 'div $1 $3))
+            ((cexp multiply bexp) (list 'mul $1 $3))
+            ((cexp division bexp) (list 'div $1 $3))
             ]
       
             [cexp
             ((minus cexp) (list 'minus $2))
-            ((lpar exp rpar) (list 'expinpar $2))
+            ((lpar exp rpar) (list 'par-exp $2))
             ((num) (list 'number $1))
             ((null) (list 'null $1))
             ((bool) (list 'bool $1))
             ((string) (list 'string $1))
             ((var) (list 'var $1))
             ((list) (list 'list $1))
-            ((var listmember) (list 'cflist $1 $2))
+            ((var listMember) (list 'element-of-list $1 $2))
             ]
             
             [list
-             ((lbrack listValues rbrack) (list 'listvalues $2))
-             ((lbrack rbrack) (list 'emptylist))
+             ((lbracket listValues rbracket) (list 'list-values $2))
+             ((lbracket rbracket) (list 'empty-list))
              ]
             
             [listValues
-             ((exp) (list 'lval $1))
-             ((exp comma listValues) (list 'lvals $1 $3))
+             ((exp) (list 'list-val $1))
+             ((exp comma listValues) (list 'list-vals $1 $3))
              ]
 
-            [variables
-             ((var) (list 'funcvar $1))
-             ((var comma variables) (list 'funcvars $1 $3))
-             ]
-            
-            [args
-             ((exp) (list 'arg $1))
-             ((exp comma args) (list 'args $1 $3))
-             ]
-            
-            [listmember
-             ((lbrack exp rbrack) (list 'listmember $2))
-             ((lbrack exp rbrack listmember) (list 'listmembers $2 $4))
+            [listMember
+             ((lbracket exp rbracket) (list 'list-member $2))
+             ((lbracket exp rbracket listMember) (list 'list-members $2 $4))
              ]
            
              )))
 
 
-
-(define-tokens a (num string bool var null))
-(define-empty-tokens b (eof plus minus mul div seteq eq noteq gt lt gte lte semicolon comma lbrack rbrack lpar rpar ret while dot endwhile if then else eif func lcurly rcurly))
-
 (define lex-this (lambda (lexer input) (lambda () (lexer input))))
-(define lexing (lex-this lexer_compiler (open-input-string "a = 3; b = [2, 3, 4]; return a + b")))
+(define lexing (lex-this lexer_compiler (open-input-string "a = 3; b = [2, 3, 4]; return a + b; if a == b then a = 1; b = 0 else a = b[5] end")))
 
 (let ((parser-res (parser_compiler lexing))) parser-res)
 (provide (all-defined-out))
